35ef0ba7045672320abae02328fb8937dbdd0dca
Iza <IzabelaSer@gmail.com>
2020-08-11 19:16:27 +0200

    Add simple GUI

---
+++ main.js
@@ -0,0 +1,122 @@
+// Global Variables
+const options = ['rock','paper','scissors'];
+let playerPlay = '';
+let computerPlay = '';
+let playerScore = 0;
+let computerScore = 0;
+let scores = [0,0];
+let winner;
+
+//DOM Manipulation
+const buttons = document.querySelectorAll('.selectionButtons');
+const winnerPar = document.querySelector('#results');
+const resultsDiv = document.querySelector('#showResults');
+const resultPar = document.createElement('p');
+const choicesDiv = document.querySelector('#choicesDiv');
+const choicesPar = document.createElement('p');
+choicesDiv.appendChild(choicesPar);
+resultsDiv.insertBefore(resultPar,results);
+const repeatDiv = document.createElement('div');
+const body = document.querySelector('body');
+body.appendChild(repeatDiv);
+
+
+//Game Functions
+
+//Computer's choice using math.random
+function computer(){
+    let i = Math.floor(Math.random() * Math.floor(3));
+    return options[i];
+}
+ 
+//One round play and score count
+function play(playerPlay, computerPlay){
+    if (playerPlay == computerPlay) {
+            resultPar.textContent = 'Draw!';
+            winner = 'no one';
+        }
+    else if (playerPlay == 'rock'){
+            if (computerPlay == 'paper'){
+                resultPar.textContent = `Fail!`;
+                winner = 'computer';
+                computerScore += 1;
+            }
+            else if (computerPlay == 'scissors'){
+                resultPar.textContent = `Win!`;
+                winner = 'player';
+                playerScore += 1;
+            }
+    }
+    else if (playerPlay == 'paper'){
+            if (computerPlay == 'scissors'){
+                resultPar.textContent = `Fail!`;
+                winner = 'computer'; 
+                computerScore += 1;
+            }
+            else if (computerPlay == 'rock'){
+                resultPar.textContent = `Win!`;
+                winner = 'player';
+                playerScore += 1;
+            }
+    }
+    else if (playerPlay == 'scissors'){
+            if (computerPlay == 'rock'){
+                resultPar.textContent = `Fail!`;
+                winner = 'computer';
+                computerScore += 1;
+            }
+            else if (computerPlay == 'paper'){
+                resultPar.textContent = `Win!`;
+                winner = 'player';
+                playerScore += 1;
+            }
+    }
+    choicesPar.textContent = `Your choice: ${playerPlay}  Computer\'s choice: ${computerPlay}`;
+    scores = [playerScore, computerScore];
+    winnerPar.textContent = `Player: ${playerScore}, Computer: ${computerScore}`;
+    return scores;
+}
+
+function confirmRestart(){
+    let answear = confirm('Do you want to restart?');
+    if (answear == true){
+            playerPlay = '';
+            computerPlay = '';
+            playerScore = 0;
+            computerScore = 0;
+            scores = [0,0];
+            choicesPar.textContent = ``;
+            winnerPar.textContent = ``;
+            resultPar.textContent = ``;
+            buttons.forEach((button) => {
+                button.removeEventListener('click', () => {
+                    playerPlay = button.value;
+                    game(playerPlay);
+              });
+            });
+
+    }
+    else {
+        choicesPar.textContent = `Well then, thanks for playing! If you change your mind, refresh the page!`;
+    }
+}
+
+//Actual game function
+function game(playerPlay){
+        computerPlay = computer();
+        scores = play(playerPlay,computerPlay);
+        playerScore = scores[0];
+        computerScore = scores[1];
+    if (playerScore == 5 || computerScore == 5){
+        confirmRestart();
+    }
+}
+
+//On click play
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerPlay = button.value;
+        game(playerPlay);
+  });
+});
